f86e04e3559a5c3edcd842c4df30a0e508b14a19
Chai Tze Chye <hologram38@gmail.com>
2018-08-23 17:43:23 +0800

    Update comments

--- mixins/santa.js
+++ mixins/santa.js
@@ -24,19 +24,19 @@ export default {
             const result = { matched: [], unmatched: [] }
             const matched = []
             let toMatch, toBeMatched
-                //first, cycle through each element in the array
+                //for outer loop, cycle through each element in the array
             for (let i = 0; i < theArray.length; i++) {
                 //check if current index is already taken/matched. Skip if yes
                 if (matched.indexOf(i) !== -1) continue
                 toMatch = theArray[i]
-                    //second, cycle through the array starting from "neighbouring" elements
+                    //for inner loop, cycle through the array starting from "neighbouring" elements
                 for (let y = i + 1; y < theArray.length; y++) {
                     toBeMatched = theArray[y]
                         //again, check if current index is taken/match. Skip if yes
                     if (matched.indexOf(y) !== -1) continue
                         //check if the element fulfills the criteria
                     if (toMatch.gender === toBeMatched.gender && toMatch.manager !== toBeMatched.manager) {
-                        //assign matching pairs to the result
+                        //assign matching pairs to result
                         result.matched.push([toMatch, toBeMatched])
                             //assign matching pairs to a temp array
                         matched.push(i, y)
