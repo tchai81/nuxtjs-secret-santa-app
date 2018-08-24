81cfd1a89761c04c2db38fbf70437971b1dc6352
Chai Tze Chye <hologram38@gmail.com>
2018-08-24 17:06:27 +0800

    Implement specification modal

--- components/json-input.vue
+++ components/json-input.vue
@@ -1,10 +1,17 @@
 <template>
   <section>
-    <div class="row mb-4">
-      <div class="col-md-12">
+    <div class="row mb-2">
+      <div class="col-md-6">
         <small class="helpBlock form-text textmuted">
           // INPUT STRUCTURE: [{ "name": "Emp1",  "gender": "M", "manager": "Alex" }]
         </small>
+      </div>
+      <div class="col-md-6 text-right">
+        <a href="#" @click="onShowSpecs">specs</a>
+      </div>
+    </div>
+    <div class="row mb-4">
+      <div class="col-md-12">
         <textarea 
           v-model="employees"
           :class="classObject" 
@@ -19,16 +26,22 @@
         <button @click="onProcess" class="btn btn-primary">Process</button>
       </div>
     </div>
+    <modal v-if="showSpecs" @close="showSpecs = false" />
   </section>
 </template>
 
 <script>
 import { mapMutations } from 'vuex'
 import Santa from '~/mixins/santa'
+import Modal from '~/components/modal'
 export default {
   mixins: [Santa],
+  components: {
+    Modal
+  },
   data() {
     return {
+      showSpecs: false,
       classObject: {
         'form-control': true,
         'is-invalid': false
@@ -51,6 +64,9 @@ export default {
         const shuffled = this.shuffleArray(json)
         this.setResult(this.matchPairs(shuffled))
       }
+    },
+    onShowSpecs() {
+      this.showSpecs = true
     },
     isJson(str) {
       try {
---
+++ components/modal.vue
@@ -0,0 +1,118 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper" @click="$emit('close')">
+        <div class="modal-container" @click.stop>
+
+          <div class="modal-header">
+            <slot name="header">
+              <h2>Specifications</h2>
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              <ul class="pb-4">
+                <li>An employee can only be assigned once</li>
+                <ul>
+                  <li>Emp1 is assigned to Emp2. Emp1 and Emp2 can no longer be assigned to other employees</li>
+                </ul>
+                <li>An employee can only exchange gifts with someone of the same gender</li>
+                <li>An employee can only exchange gifts with someone NOT reporting to the same manager</li>
+                <li>Should accept a JSON string as input</li>
+                <li>Output is a list employees (with their gender and manager) and their assigned secret santa</li>
+              </ul>
+              <hr>
+              <strong>Sample data</strong>
+              <div class="p-2 highlight">
+                {{ employees }}
+              </div>
+            </slot>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import Employees from '~/mock/data/employees'
+export default {
+  data() {
+    return {
+      employees: Employees
+    }
+  }
+}
+</script>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 600px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+
+.highlight {
+  background-color: #e0e0eb;
+}
+</style>
